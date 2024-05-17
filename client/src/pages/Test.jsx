b0d0c73d71ed771cc55accf30083548cb3832749
Indraudh <indraudhsarkar001@gmail.com>
2024-05-18 01:52:55 +0530

    first commit

---
+++ client/src/pages/Test.jsx
@@ -0,0 +1,8 @@
+import Defaultlayout from "../components/Defaultlayout";
+export default function Test() {
+  return (
+    <Defaultlayout>
+        <h1>This is test page</h1>
+    </Defaultlayout>
+  )
+}
